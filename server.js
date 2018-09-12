a455b68b3048eea27a3135e89a65ddbca9781e05
matevarju89 <42245800+matevarju89@users.noreply.github.com>
2018-09-12 20:20:26 +0200

    new changes

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express=require("express");
 const mongoose=require("mongoose");
+const bodyParser=require("body-parser");
+const passport=require("passport");
 
 const users=require("./routes/api/users");
 const profile=require("./routes/api/profile");
 const posts=require("./routes/api/posts");
 
 const app=express();
+
+//Body Parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 //DB config
 const db=require("./config/keys").mongoURI;
@@ -18,6 +24,12 @@ mongoose.connect(db, {useNewUrlParser: true})
 app.get("/", (req,res)=>{
 	res.send("Haliho")
 });
+
+//passport middleware
+app.use(passport.initialize());
+
+//passport config
+require("./config/passport")(passport);
 
 //Use routes
 app.use("/api/users", users);
